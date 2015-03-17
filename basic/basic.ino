f2fc9aa123d91d80d3e8783430138d1c843c0d7b
Adam Edgett <AdamEdgett@gmail.com>
2015-03-17 17:52:22 -0400

    Add direction variables to basic

--- basic/basic.ino
+++ basic/basic.ino
@@ -2,6 +2,8 @@ int SR = 6;
 int SL = 5;
 int DR = 8;
 int DL = 7;
+int FW = LOW;
+int BW = HIGH;
 
 void setup()
 {
@@ -16,8 +18,8 @@ void loop()
   int leftspeed = 255; //255 is maximum speed
   int rightspeed = 255;
   analogWrite (SR,255);
-  digitalWrite(DR,HIGH);
+  digitalWrite(DR,FW);
   analogWrite (SL,255);
-  digitalWrite(DL,HIGH);
+  digitalWrite(DL,FW);
   delay(100);
 }
